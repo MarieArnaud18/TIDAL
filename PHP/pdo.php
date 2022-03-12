e0e16a46c3465c5b76b753d06e7b98a22784ac77
MarieArnaud18 <72148986+MarieArnaud18@users.noreply.github.com>
2022-03-12 19:49:15 +0100

    ouf

--- PHP/pdo.php
+++ PHP/pdo.php
@@ -1,16 +1,34 @@
 <?php
 
-$user = "tidal";
+$user = "pgtidal";
 $passwd = "tidal";
 $dsn = "pgsql:host=localhost;port=5432;dbname=acudb;";
 
 try {
     $conn = new PDO($dsn,$user, $passwd);
-    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION)
-    $sql = "INSERT INTO user (test)
-    VALUES ('test')";
-    $conn->exec($sql);
-    echo "ça marche";
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    if(isset(($_POST['user']))){
+        $sql1="SELECT * FROM login  WHERE  login.login = '".$_POST['user']."'";
+        $sth1 = $conn->prepare( $sql1 );
+        $sth1->execute();
+        $data1=$sth1->fetch();
+        if($data1['username']==""){
+            $message="Erreur lors de la connexion.";
+    
+        }else{
+    
+            if($data1["motdepasse"]==$_POST['pwd']){
+                $message="connexion réussie";
+                $_SESSION['pseudo'] =$_POST['user'];
+                header('Location: ./index.php?link=page_accueil.php'); 
+            }
+    
+            else{
+                $message="Erreur lors de la connexion.";
+            }
+        }
+    }
+    
 }
 catch(PDOException $e){
     echo $sql . "<br>" . $e->getMessage();
